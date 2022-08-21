643f65f5f01645cacec86791f1c4dcdd1a110fde
saeedeh-asgharpour <saeedeh.asqarpour@gmail.com>
2022-08-21 13:13:26 +0430

    Modify Input to Form Validation

--- FirstBlazorApp/Data/Grocery.cs
+++ FirstBlazorApp/Data/Grocery.cs
@@ -1,8 +1,14 @@
-﻿namespace FirstBlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+namespace FirstBlazorApp.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15,ErrorMessage = "Name should be less than 15 character")]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1,10000, ErrorMessage = "Valid Price range is 1 to 10000")]
         public float Price { get; set; }         
     }
 }
